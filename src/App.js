ecd80a19bc4fbbe83d121917d5fa25deafcc119d
fhira21 <fhiramaulani2105@gmail.com>
2023-07-24 19:27:17 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserList from "./components/UserList";
 import AddUser from "./components/AddUser";
 import EditUser from "./components/EditUser";
-import LoginForm from "./components/login";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="container">
         <Routes>
-          <Route path="/" element={<LoginForm />} />
-          <Route path="/home" element={<UserList />} />
+          <Route path="/" element={<UserList />} />
           <Route path="add" element={<AddUser />} />
           <Route path="edit/:id" element={<EditUser />} />
         </Routes>
